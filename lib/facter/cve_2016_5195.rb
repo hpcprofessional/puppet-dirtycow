59217e0b1859dc7c816e31484bce42cd00974a0e
Trev Harmon <trevharmon@gmail.com>
2016-12-05 20:52:29 -0700

    refactor to use symbols
    `puppet apply` on a partial catalog was complaining with the use of
    Ruby constants, so we've moved over to using symbols and variables.

--- lib/facter/cve_2016_5195.rb
+++ lib/facter/cve_2016_5195.rb
@@ -1,46 +1,41 @@
 require 'puppet/util/package'
 
 Facter.add(:cve_2016_5195) do
-  confine :kernel => 'linux'
-
-  # Set constants
-  VULNERABLE     = 'vulnerable'
-  NOT_VULNERABLE = 'not_vulnerable'
-  UNKNOWN        = 'unknown'
+  confine kernel => 'linux'
 
   # Get facts
-  MAJOR_VERSION = Facter.value(:kernelmajversion)
-  VERSION       = Facter.value(:kernelversion)
+  kernel_major_version = Facter.value(kernelmajversion)
+  kernel_version       = Facter.value(kernelversion)
 
-  result = UNKNOWN
-  case :MAJOR_VERSION
+  result = :unknown
+  case :kernel_major_version
   when '4.4'
-    if Puppet::Util::Package.versioncmp(VERSION, '4.4.26') == -1
-      result = VULNERABLE
+    if Puppet::Util::Package.versioncmp(kernel_version, '4.4.26') == -1
+      result = :vulnerable
     else
-      result = NOT_VULNERABLE
+      result = :not_vulnerable
     end
   when '4.7'
-    if Puppet::Util::Package.versioncmp(VERSION, '4.7.9') == -1
-      result = VULNERABLE
+    if Puppet::Util::Package.versioncmp(kernel_version, '4.7.9') == -1
+      result = :vulnerable
     else
-      result = NOT_VULNERABLE
+      result = :not_vulnerable
     end
   when '4.8'
-    if Puppet::Util::Package.versioncmp(VERSION, '4.8.26') == -1
-      result = VULNERABLE
+    if Puppet::Util::Package.versioncmp(kernel_version, '4.8.26') == -1
+      result = :vulnerable
     else
-      result = NOT_VULNERABLE
+      result = :not_vulnerable
     end
   else
-    if Puppet::Util::Package.versioncmp(VERSION, '2.6.22') == 1
+    if Puppet::Util::Package.versioncmp(kernel_version, '2.6.22') == 1
       # The bug wasn't introduced until Kernel 2.6.22
-      result = NOT_VULNERABLE
-    elsif Puppet::Util::Package.versioncmp(MAJOR_VERSION, '4.8') == 1
+      result = :not_vulnerable
+    elsif Puppet::Util::Package.versioncmp(kernel_major_version, '4.8') == 1
       # Newer versions of the kernel should not be susceptible
-      result = NOT_VULNERABLE
+      result = :not_vulnerable
     else
-      result = VULNERABLE
+      result = :vulnerable
     end
   end
   result
